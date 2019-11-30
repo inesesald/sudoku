5c329815d2098a7cab6118fc1d345976d7560f78
Krista-tech <58356531+Krista-tech@users.noreply.github.com>
2019-11-30 10:47:30 +0200

    Initial commit

---
+++ src/Sudoku1.java
@@ -0,0 +1,39 @@
+public class Sudoku1 {
+         public static void printBoard() {
+
+            //private char[][] laukums;
+
+            // public SudokuResult() {
+            char[][] laukums = new char[9][9];
+            int row;
+            int col;
+            int number;
+            // palaistSpelesLaukumu();
+
+            //public void printBoard () {
+            System.out.println("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+            for (int i = 0; i < 9; i++) {
+
+
+                for (int j = 0; j < 9; j++) {
+                    if (j == 2 || j == 5 || j == 8) {
+                        System.out.print(laukums[i][j] + "  ▌  ");
+                    } else if (j == 0) {
+                        System.out.print("▌  " + laukums[i][j] + "  ⁞  ");
+                    } else {
+                        System.out.print(laukums[i][j] + "  ⁞  ");
+                    }
+                }
+                if (i == 2 || i == 5 || i == 8) {
+                    System.out.println();
+                    System.out.println("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+                } else {
+                    System.out.println();
+                    System.out.println("…………………………………………………………………………………………………………………………………………………");
+                }
+
+            }
+        }
+    }
+}
